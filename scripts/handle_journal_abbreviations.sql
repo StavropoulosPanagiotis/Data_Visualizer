27e0a84b44439489c73b6dd7ce230e602fca0658
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2026-04-23 17:52:23 +0300

    Database Setup Scripts

---
+++ scripts/handle_journal_abbreviations.sql
@@ -0,0 +1,60 @@
+USE `data_visualizer`;
+
+-- ----------------------------------------------------------
+-- This script is used to handle abbreviations like J. or Int. on journals
+-- The ultimate reason to do this is to match the FK's of publications to journals
+-- Now we create a function that normalizes journal names
+-- We took care of the abbreviations with more than 1000 rows affected
+-- ----------------------------------------------------------
+
+DROP FUNCTION IF EXISTS normalize_journal;
+DELIMITER $
+CREATE FUNCTION normalize_journal(journal_name TEXT)
+RETURNS TEXT
+DETERMINISTIC
+BEGIN
+    SET journal_name = REPLACE(journal_name, 'J.', 'Journal of');
+    SET journal_name = REPLACE(journal_name, 'Trans.', 'Transactions on');
+    SET journal_name = REPLACE(journal_name, 'Int.', 'International');
+    SET journal_name = REPLACE(journal_name, 'Inf.', 'Information');
+    SET journal_name = REPLACE(journal_name, 'Comput.', 'Computing');
+    SET journal_name = REPLACE(journal_name, 'Commun.', 'Communications');
+    SET journal_name = REPLACE(journal_name, 'Syst.', 'Systems');
+    SET journal_name = REPLACE(journal_name, 'Theor.', 'Theoretical');
+    SET journal_name = REPLACE(journal_name, 'Appl.', 'Applied');
+    SET journal_name = REPLACE(journal_name, 'Electr.', 'Electronic');
+    SET journal_name = REPLACE(journal_name, 'Math.', 'Mathematical');
+    SET journal_name = REPLACE(journal_name, 'Comp.', 'Computer');
+    SET journal_name = REPLACE(journal_name, 'Knowl.', 'Knowledge');
+    SET journal_name = REPLACE(journal_name, 'Eng.', 'Engineering');
+    SET journal_name = REPLACE(journal_name, 'Artif.', 'Artificial');
+    SET journal_name = REPLACE(journal_name, 'Softw.', 'Software');
+    SET journal_name = REPLACE(journal_name, 'Ann.', 'Annals');
+    SET journal_name = REPLACE(journal_name, 'Eur.', 'European');
+    SET journal_name = REPLACE(journal_name, 'Fundam.', 'Fundamenta');
+    SET journal_name = REPLACE(journal_name, 'Comb.', 'Combinatorics');
+    SET journal_name = REPLACE(journal_name, 'Rel.', 'Relations');
+    SET journal_name = REPLACE(journal_name, 'Process.', 'Processing');
+    SET journal_name = REPLACE(journal_name, 'Med.', 'Medical');
+    SET journal_name = REPLACE(journal_name, 'Adv.', 'Advanced');
+    SET journal_name = REPLACE(journal_name, 'Cyberpsy.', 'Cyberpsychology');
+    SET journal_name = REPLACE(journal_name, 'Des.', 'Design');
+    SET journal_name = REPLACE(journal_name, 'Perform.', 'Performance');
+    SET journal_name = REPLACE(journal_name, 'Sci.', 'Science');
+    SET journal_name = REPLACE(journal_name, 'Oper.', 'Operations');
+    SET journal_name = REPLACE(journal_name, 'Sig.', 'Signal');
+    SET journal_name = REPLACE(journal_name, 'Geosci.', 'Geosciences');
+    SET journal_name = REPLACE(journal_name, 'Struct.', 'Structures');
+    SET journal_name = REPLACE(journal_name, 'Mach.', 'Machine');
+    RETURN journal_name;
+END$
+DELIMITER ;
+
+-- ----------------------------------------------------------
+-- NORMALIZE STAGING DATA IN PLACE
+-- ----------------------------------------------------------
+SET SQL_SAFE_UPDATES = 0;
+UPDATE `staging_publications`
+SET `journal` = normalize_journal(`journal`)
+WHERE `type` = 'journal';
+SET SQL_SAFE_UPDATES = 1;
---
+++ scripts/load_data.sql
@@ -0,0 +1,171 @@
+USE `data_visualizer`;
+
+-- ------------------------------------------------------------
+-- Load Dimension Tables
+-- ------------------------------------------------------------
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/author_dim.csv'
+INTO TABLE `authors`
+FIELDS OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@from_file_author_name)
+SET `author_name` = TRIM(@from_file_author_name);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/conference_dim.csv'
+INTO TABLE `conferences`
+FIELDS TERMINATED BY ';'
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@from_file_title, @from_file_acronym, @from_file_rank, @from_file_primary_for)
+SET
+    `title`       = TRIM(@from_file_title),
+    `acronym`     = TRIM(@from_file_acronym),
+    `rank`        = TRIM(@from_file_rank),
+    `primary_for` = TRIM(@from_file_primary_for);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/journal_dim.csv'
+INTO TABLE `journals`
+FIELDS TERMINATED BY ';'
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@from_file_rank, @from_file_title, @from_file_oa_status, @from_file_country, @from_file_sjr_index, @from_file_citescore,
+ @from_file_h_index, @from_file_best_quartile, @from_file_best_categories, @from_file_best_subject_area,
+ @from_file_best_subject_rank, @from_file_publisher, @from_file_core_collection, @from_file_coverage,
+ @from_file_active, @from_file_in_press, @from_file_iso_language_code)
+SET
+    `rank`              = TRIM(@from_file_rank),
+    `title`             = TRIM(@from_file_title),
+    `oa_status`         = TRIM(@from_file_oa_status),
+    `country`           = TRIM(@from_file_country),
+    `sjr_index`         = TRIM(@from_file_sjr_index),
+    `citescore`         = TRIM(@from_file_citescore),
+    `h_index`           = TRIM(@from_file_h_index),
+    `best_quartile`     = TRIM(@from_file_best_quartile),
+    `best_categories`   = TRIM(@from_file_best_categories),
+    `best_subject_area` = TRIM(@from_file_best_subject_area),
+    `best_subject_rank` = TRIM(@from_file_best_subject_rank),
+    `publisher`         = TRIM(@from_file_publisher),
+    `core_collection`   = TRIM(@from_file_core_collection),
+    `coverage`          = TRIM(@from_file_coverage),
+    `active`            = TRIM(@from_file_active),
+    `in_press`          = TRIM(@from_file_in_press),
+    `iso_language_code` = TRIM(@from_file_iso_language_code);
+
+-- ------------------------------------------------------------
+-- Load Staging Tables
+-- ------------------------------------------------------------
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/publication_fact.csv'
+INTO TABLE `staging_publications`
+FIELDS TERMINATED BY ';'
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(`id`, `title`, `year`, `pages`, `ee`, `journal`, `booktitle`, `type`);
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/publication_author.csv'
+INTO TABLE `staging_publications_authors`
+FIELDS TERMINATED BY ';'
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(`publication_id`, `author_name`);
+
+-- ------------------------------------------------------------
+-- Build Staging Indexes
+-- ------------------------------------------------------------
+
+ALTER TABLE `staging_publications`
+    ADD INDEX `staging_journal_idx`   (`journal`),
+    ADD INDEX `staging_booktitle_idx` (`booktitle`);
+
+ALTER TABLE `staging_publications_authors`
+    ADD INDEX `staging_publication_id_idx` (`publication_id`),
+    ADD INDEX `staging_author_name_idx`    (`author_name`);
+
+-- ------------------------------------------------------------
+-- Build Temp Lookup Tables
+-- ------------------------------------------------------------
+
+DROP TEMPORARY TABLE IF EXISTS `tmp_journal_lookup`;
+CREATE TEMPORARY TABLE `tmp_journal_lookup` (
+    `journal_id` INT,
+    `title`      VARCHAR(500),
+    INDEX `idx_tmp_journal_title` (`title`(500))
+) SELECT MIN(`journal_id`) AS `journal_id`, `title`
+  FROM `journals`
+  GROUP BY `title`;
+
+DROP TEMPORARY TABLE IF EXISTS `tmp_conference_title_lookup`;
+CREATE TEMPORARY TABLE `tmp_conference_title_lookup` (
+    `conference_id` INT,
+    `title`         VARCHAR(500),
+    INDEX `idx_tmp_conf_title` (`title`(500))
+) SELECT MIN(`conference_id`) AS `conference_id`, `title`
+  FROM `conferences`
+  GROUP BY `title`;
+
+DROP TEMPORARY TABLE IF EXISTS `tmp_conference_acronym_lookup`;
+CREATE TEMPORARY TABLE `tmp_conference_acronym_lookup` (
+    `conference_id` INT,
+    `acronym`       VARCHAR(100),
+    INDEX `idx_tmp_conf_acronym` (`acronym`)
+) SELECT MIN(`conference_id`) AS `conference_id`, `acronym`
+  FROM `conferences`
+  GROUP BY `acronym`;
+
+-- ------------------------------------------------------------
+-- Insert into publications
+-- ------------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS = 0;
+SET UNIQUE_CHECKS = 0;
+
+INSERT INTO `publications` (`publication_id`, `type`, `title`, `year`, `pages`, `ee`, `journal_id`, `conference_id`)
+SELECT
+    sp.`id`,
+    sp.`type`,
+    sp.`title`,
+    sp.`year`,
+    sp.`pages`,
+    sp.`ee`,
+    jd.`journal_id`,
+    COALESCE(cd1.`conference_id`, cd2.`conference_id`)
+FROM `staging_publications` sp
+LEFT JOIN `tmp_journal_lookup` jd
+    ON sp.`journal` = jd.`title` AND sp.`type` = 'journal'
+LEFT JOIN `tmp_conference_title_lookup` cd1
+    ON sp.`booktitle` = cd1.`title` AND sp.`type` = 'conference'
+LEFT JOIN `tmp_conference_acronym_lookup` cd2
+    ON sp.`booktitle` = cd2.`acronym` AND sp.`type` = 'conference';
+
+DROP TEMPORARY TABLE `tmp_journal_lookup`;
+DROP TEMPORARY TABLE `tmp_conference_title_lookup`;
+DROP TEMPORARY TABLE `tmp_conference_acronym_lookup`;
+
+-- ------------------------------------------------------------
+-- Insert into publications_authors
+-- ------------------------------------------------------------
+
+INSERT INTO `publications_authors` (`publication_id`, `author_id`)
+SELECT DISTINCT
+    spa.`publication_id`,
+    a.`author_id`
+FROM `staging_publications_authors` spa
+JOIN `publications` p
+    ON spa.`publication_id` = p.`publication_id`
+JOIN `authors` a
+    ON spa.`author_name` = a.`author_name`;
+
+SET UNIQUE_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- ------------------------------------------------------------
+-- Drop Staging Tables
+-- ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `staging_publications`;
+DROP TABLE IF EXISTS `staging_publications_authors`;
