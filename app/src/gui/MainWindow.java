a5d240e5c3525daa923a27c2f7b0bc5255e19657
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2026-05-04 19:40:06 +0300

    App's main window

---
+++ app/src/gui/MainWindow.java
@@ -0,0 +1,28 @@
+package gui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+public class MainWindow {
+
+	private final TabPane root;
+
+	public MainWindow() throws Exception {
+		root = new TabPane();
+		Tab authorsTab = new Tab("Authors", load("/gui/view/AuthorView.fxml"));
+		Tab venuesTab = new Tab("Venues", load("/gui/view/VenueView.fxml"));
+		Tab yearsTab = new Tab("Years", load("/gui/view/YearView.fxml"));
+		Tab chartsTab = new Tab("Charts", load("/gui/view/ChartsView.fxml"));
+		for (Tab tab : new Tab[]{authorsTab, venuesTab, yearsTab, chartsTab})
+			tab.setClosable(false);
+		root.getTabs().addAll(authorsTab, venuesTab, yearsTab, chartsTab);
+	}
+
+	private Parent load(String fxml) throws Exception {
+		return FXMLLoader.load(getClass().getResource(fxml));
+	}
+
+	public TabPane getRoot() { return root; }
+}
