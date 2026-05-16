9f66ea43a9d6fd0fdc3a900f60e0131559909df9
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2026-05-16 16:25:34 +0300

    add privileges to new user

--- scripts/create_user.sql
+++ scripts/create_user.sql
@@ -5,5 +5,7 @@
 CREATE USER IF NOT EXISTS 'DataVisualizerUser'@'localhost' IDENTIFIED BY 'DataVisualizer';
 
 GRANT ALL PRIVILEGES ON `data_visualizer`.* TO 'DataVisualizerUser'@'localhost';
+GRANT SYSTEM_VARIABLES_ADMIN ON *.* TO 'DataVisualizerUser'@'localhost';
+GRANT FILE ON *.* TO 'DataVisualizerUser'@'localhost';
 
 FLUSH PRIVILEGES;
