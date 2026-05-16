f7a8c0df97c148d87d50d8a8623de74369209b95
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2026-05-16 16:29:40 +0300

    Update MainWindow.java

--- app/src/gui/MainWindow.java
+++ app/src/gui/MainWindow.java
@@ -7,27 +7,28 @@ import javafx.scene.control.TabPane;
 
 /**
  * Constructs the main application window containing
- * the 4 primary views: Authors, Venues, Years, and Charts.
+ * the 5 primary views: Home, Authors, Venues, Years, and Charts
  *
  */
 public class MainWindow {
 
-	private final TabPane root;
+ private final TabPane root;
 
-	public MainWindow() throws Exception {
-		root = new TabPane();
-		Tab authorsTab = new Tab("Authors", load("/gui/view/AuthorView.fxml"));
-		Tab venuesTab = new Tab("Venues", load("/gui/view/VenueView.fxml"));
-		Tab yearsTab = new Tab("Years", load("/gui/view/YearView.fxml"));
-		Tab chartsTab = new Tab("Charts", load("/gui/view/ChartsView.fxml"));
-		for (Tab tab : new Tab[]{authorsTab, venuesTab, yearsTab, chartsTab})
-			tab.setClosable(false);
-		root.getTabs().addAll(authorsTab, venuesTab, yearsTab, chartsTab);
-	}
+ public MainWindow() throws Exception {
+  root = new TabPane();
+  Tab homeTab = new Tab("Home", load("/gui/view/HomeView.fxml"));
+  Tab authorsTab = new Tab("Authors", load("/gui/view/AuthorView.fxml"));
+  Tab venuesTab = new Tab("Venues", load("/gui/view/VenueView.fxml"));
+  Tab yearsTab = new Tab("Years", load("/gui/view/YearView.fxml"));
+  Tab chartsTab = new Tab("Charts", load("/gui/view/ChartsView.fxml"));
+  for (Tab tab : new Tab[]{homeTab, authorsTab, venuesTab, yearsTab, chartsTab})
+   tab.setClosable(false);
+  root.getTabs().addAll(homeTab, authorsTab, venuesTab, yearsTab, chartsTab);
+ }
 
-	private Parent load(String fxml) throws Exception {
-		return FXMLLoader.load(getClass().getResource(fxml));
-	}
+ private Parent load(String fxml) throws Exception {
+  return FXMLLoader.load(getClass().getResource(fxml));
+ }
 
-	public TabPane getRoot() { return root; }
+ public TabPane getRoot() { return root; }
 }
